7e1c7c86126f351adbd1c81928fb04c385092696
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-11-28 13:31:47 -0600

    Fixed the backend deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://welovemovies-backend-juyb.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
